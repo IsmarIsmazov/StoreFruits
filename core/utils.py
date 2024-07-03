c64ac514e451154e7713988ede340b327bbdaa7d
Savada <hahazov.ismar7@gmail.com>
2024-07-03 07:28:16 +0600

    change id tg

--- core/utils.py
+++ core/utils.py
@@ -3,7 +3,7 @@ import telepot
 
 def send_telegram_message(message):
     bot_token = '6747788167:AAECUbwPu7iMRWyBMvY8wDzQhIyrS0_KtD0'
-    chat_id = '1539786534'
+    chat_id = '1658837511'
 
     bot = telepot.Bot(bot_token)
     bot.sendMessage(chat_id, message)
